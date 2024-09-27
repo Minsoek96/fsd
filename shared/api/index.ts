29890c649e6d15fabce364ce8f2a8cd4722ee9e7
Minsoek96 <FE.minseok96@gmail.com>
2024-09-27 23:01:40 +0900

    Feat Authentication

--- shared/api/index.ts
+++ shared/api/index.ts
@@ -1,2 +1,4 @@
 export { GET, POST, PUT, DELETE } from "./client";
+
 export type { Article } from "./models";
+export { createUserSession, getUserFromSession, requireUser } from "./auth.server";
--- shared/api/models.ts
+++ shared/api/models.ts
@@ -2,3 +2,4 @@
 import type { components } from "./v1";
 
 export type Article = components["schemas"]["Article"];
+export type User = components["schemas"]["User"];
